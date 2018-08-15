b20323ba26c991bb2c7e6ec6553d4f9fa6f4b6f7
Marika Ross <marika.ross@gmail.com>
2018-08-15 16:56:30 -0600

    Change get projects function to getPalettes

--- public/scripts.js
+++ public/scripts.js
@@ -3,11 +3,11 @@ $('.lock').on('click', toggleLock);
 $('.save').on('click', savePalette);
 $('.save-project').on('submit', saveProject);
 
-$(window).on('load', makePalette);
+$(window).on('load', welcome);
 
 function welcome() {
-  getProjects()
   makePalette()
+  getProjects()
 }
 
 
@@ -22,7 +22,7 @@ function getAColor() {
 }
 
 function makePalette() {
-  console.log('hi')
+  // console.log('hi')
   for (var i = 1; i < 6; i++) {
     if(!$(this).children(`.color${[i]}`).hasClass('.locked-color')) {
       var color = getAColor()
@@ -63,32 +63,33 @@ function postProject(project) {
   })
 }
 
-// function getProjects() {
-//   fetch('http://localhost:3000/api/v1/projects')
-//     .then(response => response.json())
-//     .then(result => showProjects(results))
-//     .catch(error => throw error)
+function getProjects() {
+  return fetch('http://localhost:3000/api/v1/projects')
+    .then(response => response.json())
+    .then(result => getPalettes(result))
+    .catch(error => console.log(error))
+}
 
-// }
 
-// function showProjects(results) {
-//   results.map(result => {
-//     return 
-//   $('.projects').append(`
-//     <h2>${result.name}</h2>
-//       <h3>palette name</h3>
-//       <section class='tiny-palette'>
-//         <article>color[0]</article>
-//         <article>color[1]</article>
-//         <article>color[2]</article>
-//         <article>color[3]</article>
-//         <article>color[4]</article>
-//       </section>
-//       `)
-//   })
-// }
 
-function getPalette() {
+function showProjects(results) {
+  results.forEach(result => {
+    return $('.projects').append(`
+      <h2>${result.name}</h2>
+      <h3>palette name</h3>
+      <section class='tiny-palette'>
+        <article>color[0]</article>
+        <article>color[1]</article>
+        <article>color[2]</article>
+        <article>color[3]</article>
+        <article>color[4]</article>
+      </section>
+      `)
+  })
+}
+
+function getPalettes() {
+//iterate through projects and fetch corresponding palettes
 
 }
 
