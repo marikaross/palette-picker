bbd25a4c916e8610d7a06ffce5fd4fba5aa5b865
Marika Ross <marika.ross@gmail.com>
2018-08-18 16:50:23 -0600

    Add test for getting single palette

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -34,8 +34,8 @@ describe('GET to /api/v1/projects', () => {
       response.body[0].project_name.should.equal('Party Time');
       response.body[0].should.have.property('id');
       response.body[0].id.should.equal(1);
+      done()
     })
-    done()
   })
 
   it('should return a 404 for a route that does not exist', done => {
@@ -43,8 +43,8 @@ describe('GET to /api/v1/projects', () => {
     .get('/sad')
     .end((err, response) => {
       response.should.have.status(404);
-    })
     done()
+    })
   })
 })
 
@@ -102,8 +102,8 @@ describe ('GET to /api/v1/palettes', () => {
       response.body[1].project_id.should.equal(1);
       response.body[1].should.have.property('id');
       response.body[1].id.should.equal(2);
+      done();
     })
-    done();
   })
 
    it('should return a 404 for a route that does not exist', done => {
@@ -111,8 +111,8 @@ describe ('GET to /api/v1/palettes', () => {
     .get('/sad')
     .end((err, response) => {
       response.should.have.status(404);
-    })
     done()
+    })
   })
 })
 
@@ -142,8 +142,8 @@ describe('POST /api/v1/projects', () => {
       response.body.should.be.a('object')
       response.body.should.have.property('id')
       response.body.id.should.equal(2)
-    })
     done()
+    })
   })
 
   it('should not create an entry if data is missing', done => {
@@ -154,8 +154,8 @@ describe('POST /api/v1/projects', () => {
     .end((err, response) => { 
       response.should.have.status(422)
       response.body.error.should.equal('Expected format: { name: <STRING> }. You are missing a "project_name" property.')
-    })
     done()
+    })
   })
 })
 
@@ -192,8 +192,8 @@ describe('POST /api/v1/projects', () => {
       response.body.should.be.a('object')
       response.body.should.have.property('id')
       response.body.id.should.equal(3)
-    })
     done()
+    })
   })
 
   it('should not create an entry if data is missing', done => {
@@ -212,16 +212,29 @@ describe('POST /api/v1/projects', () => {
     .end((err, response) => { 
       response.should.have.status(422)
       response.body.error.should.equal('Expected format: {name: <STRING>, color_1: <STRING>, color_2: <STRING>, color_3: <STRING>, color_4: <STRING>, color_5: <STRING>, project_id: <NUMBER>}. You are missing a "palette_name" property.')
-    })
     done()
+    })
   })
 })
 
 describe('GET to /api/v1/projects/:id', () => {
+    beforeEach( done => {
+    knex.migrate.rollback()
+    .then(() => {
+      knex.migrate.latest()
+      .then(() => {
+        return knex.seed.run()
+        .then(() =>{
+          done()
+        })
+      })
+    })
+  })
   it('should return a single project', done => {
     chai.request(server)
     .get('/api/v1/projects/1')
-    response.should.have.status(200);
+    .end((err, response) => {
+      response.should.have.status(200);
       response.should.be.json;
       response.body.should.be.a('array');
       response.body.length.should.equal(1);
@@ -229,7 +242,53 @@ describe('GET to /api/v1/projects/:id', () => {
       response.body[0].project_name.should.equal('Party Time');
       response.body[0].should.have.property('id');
       response.body[0].id.should.equal(1);
+
     })
     done()
+  })
+
+   it('should return a 404 for a route that does not exist', done => {
+    chai.request(server)
+    .get('/sad')
+    .end((err, response) => {
+      response.should.have.status(404);
+    done()
+    })
+  })
+})
+
+describe('GET /api/v1/palettes/:id', () => {
+  it('should return a single palette', done => {
+    chai.request(server)
+    .get('/api/v1/palettes/1')
+    .end((err, response) => {
+      response.should.have.status(200);
+      response.should.be.json;
+      response.body.should.be.a('array');
+      response.body.length.should.equal(1);
+      response.body[0].should.have.property('palette_name');
+      response.body[0].palette_name.should.equal('best');
+      response.body[0].should.have.property('color_1');
+      response.body[0].color_1.should.equal('#30E8A7');
+      response.body[0].should.have.property('color_2');
+      response.body[0].color_2.should.equal('#FC68D1');
+      response.body[0].should.have.property('color_3');
+      response.body[0].color_3.should.equal('#A0CE39');
+      response.body[0].should.have.property('color_4');
+      response.body[0].color_4.should.equal('#D5E403');
+      response.body[0].should.have.property('color_5');
+      response.body[0].color_5.should.equal('#819ADD');
+      response.body[0].should.have.property('project_id');
+      response.body[0].project_id.should.equal(1);
+      response.body[0].should.have.property('id');
+      response.body[0].id.should.equal(1);
+      done()
+    })
+  })
+})
+
+describe('DELETE api/v1/palettes/:id', () => {
+  it('should delete a single palette from the database', done => {
+    
   })
 })
