52e44a6b031843dd8cebcc92f98a4f1258144781
Marika Ross <marika.ross@gmail.com>
2018-08-18 12:58:53 -0600

    Write sad path tests for GET requests

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -37,6 +37,15 @@ describe('GET to /api/v1/projects', () => {
     })
     done()
   })
+
+  it('should return a 404 for a route that does not exist', done => {
+    chai.request(server)
+    .get('/sad')
+    .end((err, response) => {
+      response.should.have.status(404);
+    })
+    done()
+  })
 })
 
 describe ('GET to /api/v1/palettes', () => {
@@ -61,39 +70,48 @@ describe ('GET to /api/v1/palettes', () => {
       response.should.be.json;
       response.body.should.be.a('array');
       response.body.length.should.equal(2);
-      response.body[0].should.have.property(palette_name);
-      response.body[0].should.palette_name.should.equal('best');
-      response.body[0].should.have.property(color_1);
+      response.body[0].should.have.property('palette_name');
+      response.body[0].palette_name.should.equal('best');
+      response.body[0].should.have.property('color_1');
       response.body[0].color_1.should.equal('#30E8A7');
-      response.body[0].should.have.property(color_2);
+      response.body[0].should.have.property('color_2');
       response.body[0].color_2.should.equal('#FC68D1');
-      response.body[0].should.have.property(color_3);
+      response.body[0].should.have.property('color_3');
       response.body[0].color_3.should.equal('#A0CE39');
-      response.body[0].should.have.property(color_4);
+      response.body[0].should.have.property('color_4');
       response.body[0].color_4.should.equal('#D5E403');
-      response.body[0].should.have.property(color_5);
+      response.body[0].should.have.property('color_5');
       response.body[0].color_5.should.equal('#819ADD');
       response.body[0].should.have.property('project_id');
-      response.body[0].project_id.should.equal(2);
-      response.body[0].should.have.property(id);
+      response.body[0].project_id.should.equal(1);
+      response.body[0].should.have.property('id');
       response.body[0].id.should.equal(1);
-      response.body[1].should.have.property(palette_name);
-      response.body[1].should.palette_name.should.equal('so good');
-      response.body[1].should.have.property(color_1);
+      response.body[1].should.have.property('palette_name');
+      response.body[1].palette_name.should.equal('so good');
+      response.body[1].should.have.property('color_1');
       response.body[1].color_1.should.equal('#30E8A7');
-      response.body[1].should.have.property(color_2);
+      response.body[1].should.have.property('color_2');
       response.body[1].color_2.should.equal('#FC68D1');
-      response.body[1].should.have.property(color_3);
+      response.body[1].should.have.property('color_3');
       response.body[1].color_3.should.equal('#A0CE39');
-      response.body[1].should.have.property(color_4);
+      response.body[1].should.have.property('color_4');
       response.body[1].color_4.should.equal('#D5E403');
-      response.body[1].should.have.property(color_5);
+      response.body[1].should.have.property('color_5');
       response.body[1].color_5.should.equal('#819ADD');
       response.body[1].should.have.property('project_id');
-      response.body[1].project_id.should.equal(2);
-      response.body[1].should.have.property(id);
-      response.body[1].id.should.equal(1);
+      response.body[1].project_id.should.equal(1);
+      response.body[1].should.have.property('id');
+      response.body[1].id.should.equal(2);
     })
     done();
+  })
+
+   it('should return a 404 for a route that does not exist', done => {
+    chai.request(server)
+    .get('/sad')
+    .end((err, response) => {
+      response.should.have.status(404);
+    })
+    done()
   })
 })
