a191439341f9d56d0d231f44051843d3618141f5
Marika Ross <marika.ross@gmail.com>
2018-08-18 12:46:13 -0600

    Add beforeEach hook to each GET happy path test

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -4,10 +4,24 @@ const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
 const server = require('../server');
+const knex = require('../db/knex');
 
 chai.use(chaiHttp);
 
 describe('GET to /api/v1/projects', () => {
+  beforeEach( done => {
+    knex.migrate.rollback()
+    .then(() => {
+      knex.migrate.latest()
+      .then(() => {
+        return knex.seed.run()
+        .then(() =>{
+          done()
+        })
+      })
+    })
+  })
+
   it('should return all projects', done => {
     chai.request(server)
     .get('/api/v1/projects')
@@ -16,7 +30,7 @@ describe('GET to /api/v1/projects', () => {
       response.should.be.json;
       response.body.should.be.a('array');
       response.body.length.should.equal(1);
-      response.body[0].should.have.property(project_name);
+      response.body[0].should.have.property('project_name');
       response.body[0].project_name.should.equal('Party Time');
       response.body[0].should.have.property('id');
       response.body[0].id.should.equal(1);
@@ -26,6 +40,19 @@ describe('GET to /api/v1/projects', () => {
 })
 
 describe ('GET to /api/v1/palettes', () => {
+  beforeEach( done => {
+    knex.migrate.rollback()
+    .then(() => {
+      knex.migrate.latest()
+      .then(() => {
+        return knex.seed.run()
+        .then(() =>{
+          done()
+        })
+      })
+    })
+  })
+
   it('should return all palettes', done => {
     chai.request(server)
     .get('/api/v1/palettes')
