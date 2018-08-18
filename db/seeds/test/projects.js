623acbd6fe393707143779aaf4e01d04f7511c2e
Marika Ross <marika.ross@gmail.com>
2018-08-18 12:17:31 -0600

    Set up test database and write get test for projects

--- db/seeds/test/projects.js
+++ db/seeds/test/projects.js
@@ -10,7 +10,8 @@ exports.seed = function(knex, Promise) {
           }, 'id')
           .then( project => {
             return knex('palettes').insert([
-              { palette_name: 'best', color_1: '#30E8A7', color_2: '#FC68D1', color_3: '#A0CE39', color_4: '#D5E403', color_5: '#819ADD', project_id: project[0] }
+              { palette_name: 'best', color_1: '#30E8A7', color_2: '#FC68D1', color_3: '#A0CE39', color_4: '#D5E403', color_5: '#819ADD', project_id: project[0] },
+              { palette_name: 'so good', color_1: '#30E8A7', color_2: '#FC68D1', color_3: '#A0CE39', color_4: '#D5E403', color_5: '#819ADD', project_id: project[0] }
             ])
           })
           .then(() => console.log('Seeding complete!'))
--- test/routes.spec.js
+++ test/routes.spec.js
@@ -1,3 +1,5 @@
+process.env.NODE_ENV = 'test';
+
 const chai = require('chai');
 const should = chai.should();
 const chaiHttp = require('chai-http');
@@ -8,10 +10,23 @@ chai.use(chaiHttp);
 describe('GET to /api/v1/projects', () => {
   it('should return all projects', done => {
     chai.request(server)
-    .get('/api/v1/students')
+    .get('/api/v1/projects')
     .end((err, response) => {
       response.should.have.status(200);
+      response.should.be.json;
+      response.body.should.be.a('array');
+      response.body.length.should.equal(1);
+      response.body[0].should.have.property(project_name);
+      response.body[0].project_name.should.equal('Party Time');
+      response.body[0].should.have.property('id');
+      response.body[0].id.should.equal(1);
     })
+    done()
+  })
+})
+
+describe ('GET to /api/v1/palettes' () => {
+  it('should return all palettes', done => {
     done()
   })
 })
