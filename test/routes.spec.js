a108b7a4ca373f5d86843eacadb3e270028e1dc8
Marika Ross <marika.ross@gmail.com>
2018-08-18 18:12:36 -0600

    Write delete test

--- test/routes.spec.js
+++ test/routes.spec.js
@@ -288,7 +288,50 @@ describe('GET /api/v1/palettes/:id', () => {
 })
 
 describe('DELETE api/v1/palettes/:id', () => {
+  beforeEach( done => {
+    knex.migrate.rollback()
+    .then(() => {
+      knex.migrate.latest()
+      .then(() => {
+        return knex.seed.run()
+        .then(() =>{
+          done()
+        })
+      })
+    })
+  })
+
   it('should delete a single palette from the database', done => {
-    
+    chai.request(server)
+    .delete('/api/v1/palettes/2')
+    .end((err, response) => {
+      response.should.have.status(200)
+      response.should.be.json
+      chai.request(server)
+      .get('/api/v1/palettes')
+      .end((err, response) => {
+        response.should.have.status(200);
+        response.should.be.json;
+        response.body.should.be.a('array');
+        response.body.length.should.equal(2);
+        response.body[0].should.have.property('palette_name');
+        response.body[0].palette_name.should.equal('best');
+        response.body[0].should.have.property('color_1');
+        response.body[0].color_1.should.equal('#30E8A7');
+        response.body[0].should.have.property('color_2');
+        response.body[0].color_2.should.equal('#FC68D1');
+        response.body[0].should.have.property('color_3');
+        response.body[0].color_3.should.equal('#A0CE39');
+        response.body[0].should.have.property('color_4');
+        response.body[0].color_4.should.equal('#D5E403');
+        response.body[0].should.have.property('color_5');
+        response.body[0].color_5.should.equal('#819ADD');
+        response.body[0].should.have.property('project_id');
+        response.body[0].project_id.should.equal(1);
+        response.body[0].should.have.property('id');
+        response.body[0].id.should.equal(1);
+      })
+      done()
+    })
   })
 })
